9e59aff2b0082a2a2385608326434e30221c4615
memicelis <elismemic5@gmail.com>
2023-11-28 13:40:21 +0100

    Add author_id as foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_122933) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_28_123713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_122933) do
     t.datetime "updated_at", null: false
     t.integer "comments_counter"
     t.integer "likes_counter"
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_122933) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
