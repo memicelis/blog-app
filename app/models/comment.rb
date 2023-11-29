5d6aa7fa3ca3790ed6b8d64bfc03bb4c4be6eced
memicelis <elismemic5@gmail.com>
2023-11-29 15:44:57 +0100

    Add methods for updating counts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   def update_comments_counter
-    update(comments_counter: comments.count)
+    post.increment!(:comments_counter)
   end
+
+  after_save :update_comments_counter
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  def update_post_likes_counter
+    post.increment!(:likes_counter)
+  end
+
+  after_save :update_post_likes_counter
 end
