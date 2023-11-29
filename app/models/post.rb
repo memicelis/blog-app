199bdcebe868d146368e764fbcbee66ecf49aaa2
memicelis <elismemic5@gmail.com>
2023-11-29 15:26:00 +0100

    Add methods for updating posts counter and for returning most recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  def update_posts_counter
+    update(posts_counter: posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at :desc).limit(5)
+  end
 end
