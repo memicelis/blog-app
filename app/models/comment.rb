eb25db269a21a88b408352687dd92f4a49ca9fa9
memicelis <elismemic5@gmail.com>
2023-12-08 14:20:23 +0100

    Add minimum length of comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
+  validates :text, length: { minimum: 1 }
+
   def update_comments_counter
     post.increment!(:comments_counter)
   end
