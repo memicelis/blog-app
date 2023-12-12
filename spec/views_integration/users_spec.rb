e102e960f73a93c3f8244538ebd973c3c3413304
memicelis <elismemic5@gmail.com>
2023-12-12 16:17:54 +0100

    Fix rubocop linters

--- spec/views_integration/users_spec.rb
+++ spec/views_integration/users_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :system do
-    subject { User.new(name: 'User1', photo: 'https://avatars.githubusercontent.com/u/7747059?v=4', posts_counter: 6, bio: 'Full-stack web developer') }
+  subject { User.new(name: 'User1', photo: 'https://avatars.githubusercontent.com/u/7747059?v=4', posts_counter: 6, bio: 'Full-stack web developer') }
 
-    before { subject.save }
+  before { subject.save }
 
   it 'I can see the username, profile picture, and post count for each user on user#index page' do
     visit users_path
@@ -11,7 +11,6 @@ RSpec.describe User, type: :system do
     expect(page).to have_content(subject.name)
     expect(page).to have_css("img[src*='#{subject.photo}']")
     expect(page).to have_content(subject.posts_counter)
-
   end
 
   it 'When I click on user, it opens its page' do
@@ -22,13 +21,13 @@ RSpec.describe User, type: :system do
     expect(page).to have_current_path(user_path(subject))
   end
 
-  it 'On user#show, I can see user\'s profile picture,username,number of posts,bio,first 3 posts, button that lets me view all posts' do
-
-   Post.create([
-      { author: subject, title: 'First Post', text: 'My first post' },
-      { author: subject, title: 'Second Post', text: 'My Second post' },
-      { author: subject, title: 'Third Post', text: 'My Third post' },
-      { author: subject, title: 'Fourth Post', text: 'My Fourth post' }]) 
+  it 'On user#show can see user\'s profile picture,username,number of posts,bio,first 3 posts, button view all posts' do
+    Post.create([
+                  { author: subject, title: 'First Post', text: 'My first post' },
+                  { author: subject, title: 'Second Post', text: 'My Second post' },
+                  { author: subject, title: 'Third Post', text: 'My Third post' },
+                  { author: subject, title: 'Fourth Post', text: 'My Fourth post' }
+                ])
 
     visit user_path(subject)
     expect(page).to have_css("img[src*='#{subject.photo}']")
@@ -45,10 +44,11 @@ RSpec.describe User, type: :system do
 
   it 'On user#show click a user\'s post, it redirects me to that post\'s show page.' do
     post = Post.create(
-      { author: subject, title: 'First Post', text: 'My first post' })
+      { author: subject, title: 'First Post', text: 'My first post' }
+    )
     visit user_path(subject)
     click_link 'First Post'
-    expect(page).to have_current_path(user_post_path(subject,post))
+    expect(page).to have_current_path(user_post_path(subject, post))
   end
 
   it 'On user#show click to see all posts, it redirects me to user\'s post\'s index page.' do
