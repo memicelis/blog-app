70f38ba6025f412af29ec4a79a584bcde39039c3
memicelis <elismemic5@gmail.com>
2023-12-14 13:44:27 +0100

    Fix typo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  has_many :comments, dependent: destroy 
-  has_many :likes, dependent: destroy
+  has_many :comments, dependent: :destroy 
+  has_many :likes, dependent: :destroy
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   validates :title, presence: true, length: { in: 2..250 }
